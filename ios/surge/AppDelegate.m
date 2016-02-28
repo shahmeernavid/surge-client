cc1ca37a74b5c688f05c8dda99d88e967d5024b8
Shahmeer Navid <shahmeernavid@fb.com>
2016-02-28 14:39:22 -0800

    stuff

--- ios/surge/AppDelegate.m
+++ ios/surge/AppDelegate.m
@@ -8,7 +8,7 @@
  */
 
 #import "AppDelegate.h"
-
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import "RCTRootView.h"
 
 @implementation AppDelegate
@@ -53,5 +53,15 @@
   [self.window makeKeyAndVisible];
   return YES;
 }
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+  return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                        openURL:url
+                                              sourceApplication:sourceApplication
+                                                     annotation:annotation
+          ];
+}
+
+
 
 @end
